af3e2afccaa04e3c870f087df321eb2bb4643e99
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-09-05 12:02:03 +0300

    Algorithm implemetation started.

--- SaimmodOne/SaimmodOne/Program.cs
+++ SaimmodOne/SaimmodOne/Program.cs
@@ -1,13 +1,37 @@
 ﻿using System;
 
-
 namespace SaimmodOne
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int mult, 
+                init,
+                divider;
+
+            Console.WriteLine($"{nameof(mult)}");
+            mult = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{nameof(init)}");
+            init = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine($"{nameof(divider)}");
+            divider = Convert.ToInt32(Console.ReadLine());
+
+            var alg = new Algorithm(mult, init, divider);
+            var sq = alg.GetSequence();
+            var count = 0;
+            Console.WriteLine("\nVALUES\n");
+            foreach (int item in sq)
+            {
+                count++;
+                Console.WriteLine(item);
+                if (count == 20)
+                {
+                    break;
+                }
+            }
         }
     }
 }
