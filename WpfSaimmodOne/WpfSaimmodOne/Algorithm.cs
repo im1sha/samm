311a388847212e9dd99ce8be5608b182ee9dc4ea
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-09-07 13:20:24 +0300

    Migrate to WPF

---
+++ WpfSaimmodOne/WpfSaimmodOne/Algorithm.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace WpfSaimmodOne
+{
+    internal class Algorithm
+    {
+        private readonly int _multiplier;
+        private readonly int _inintialValue;
+        private readonly int _divider;
+        private readonly IEnumerable<int> _seq;
+
+        /// <summary>
+        /// R[n+1] = (R[n] *a) %m. m > a
+        /// </summary>
+        /// <param name="multiplier">a</param>
+        /// <param name="initialValue">R[0]</param>
+        /// <param name="divider">m</param>
+        public Algorithm(int multiplier, int initialValue, int divider)
+        {
+            _multiplier = multiplier;
+            _inintialValue = initialValue;
+            _divider = divider;
+            _seq = CreateSequence(multiplier, initialValue, divider);
+        }
+
+        public int Divider => _divider;
+
+        // Gets sequnce of generated members 
+        private IEnumerable<int> CreateSequence(int multiplier, int initialValue, int divider)
+        {
+            int currMember = initialValue;
+            int multiplication;
+
+            while (true)
+            {           
+                // a * R[n] 
+                multiplication = multiplier * currMember;
+                currMember = multiplication % divider;
+                yield return currMember;
+            }
+        }
+
+        public IEnumerable<int> GetSequence()
+        {
+            foreach (var item in _seq)
+            {
+                yield return item;
+            }
+        }
+    }
+}
---
+++ WpfSaimmodOne/WpfSaimmodOne/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfSaimmodOne
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+
+            //int mult,
+            //    init,
+            //    divider;
+
+            //Console.WriteLine($"{nameof(mult)}");
+            //mult = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"{nameof(init)}");
+            //init = Convert.ToInt32(Console.ReadLine());
+
+            //Console.WriteLine($"{nameof(divider)}");
+            //divider = Convert.ToInt32(Console.ReadLine());
+
+            //var alg = new Algorithm(mult, init, divider);
+            //var sq = alg.GetSequence();
+            //var count = 0;
+            //Console.WriteLine("\nVALUES\n");
+            //foreach (int item in sq)
+            //{
+            //    count++;
+            //    Console.WriteLine(item);
+            //    if (count == 20)
+            //    {
+            //        break;
+            //    }
+            //}
+        }
+    }
+}
