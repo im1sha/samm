bf9ec9416756f53f5871dce1766669a91fcc9e48
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-09-07 17:23:58 +0300

    Initial chart implemented.

--- WpfSaimmodOne/WpfSaimmodOne/MainWindow.xaml.cs
+++ WpfSaimmodOne/WpfSaimmodOne/MainWindow.xaml.cs
@@ -20,12 +20,12 @@ namespace WpfSaimmodOne
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
-    {
-      
+    {    
         public MainWindow()
         {
             InitializeComponent();
         }
+
         private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
         {
             Regex regex = new Regex("[^0-9]+");
@@ -33,29 +33,90 @@ namespace WpfSaimmodOne
         }
 
         private void Calculate(object sender, RoutedEventArgs e)
-        {            
-            string msg = string.Empty;
+        {
+            const int TOTAL_INTERVALS = 20;
+            const int TOTAL_VALUES = 500000;
 
-            var mult = Convert.ToInt32(multiplyer.Text);
-            var init = Convert.ToInt32(initialValue.Text);
-            var div = Convert.ToInt32(divider.Text);
+            int GetIntContent(string str)
+            {
+                if (!int.TryParse(str, out int res))
+                {
+                    return 0;
+                }
+                return res;
+            }
+            var mult = GetIntContent(_multiplier.Text);
+            var init = GetIntContent(_initialValue.Text);
+            var div = GetIntContent(_divider.Text);
 
-            var alg = new Algorithm(mult, init, div);
-            var sq = alg.GetSequence();
+            #region generation
+
+            IEnumerable<int> sq = new Algorithm(mult, init, div).GetSequence();
+
+            #region msgbox        
+            // var msg = string.Empty;
+            //var count = 0;
+            //foreach (int item in sq)
+            //{
+            //    count++;
+            //    msg += $"{item}\t";
+            //    if (count == 20) break;               
+            //}
+            //MessageBox.Show(msg);
+            #endregion
+            IList<int> generatedValues = new List<int>();
             var count = 0;
-
-            Console.WriteLine("\nVALUES\n");
             foreach (int item in sq)
             {
-                count++;
-                msg += $"{item}\t";
-                if (count == 20)
-                {
-                    break;
-                }
+                if (count == TOTAL_VALUES) break;
+                generatedValues.Add(item);
+                ++count;
             }
 
-            MessageBox.Show(msg);
+            #endregion
+
+            #region interval calculation
+
+            double intervalLength = (double)div / TOTAL_INTERVALS;
+
+            List<int> results = new List<int>();
+            results.AddRange(Enumerable.Repeat(0, TOTAL_INTERVALS));
+
+            foreach (var item in generatedValues)
+            {
+                results[(int)(item/intervalLength)]++;
+            }
+            #endregion
+
+            DrawBarChart(_panel, results);
+        }
+
+        void DrawBarChart(Panel target, IEnumerable<int> values)
+        {
+            target.Children.Clear();
+            
+            double heightCoef = target.ActualHeight / values.Max(); 
+            double itemWidth = target.ActualWidth / values.Count();
+
+            int totalItems = values.Count();
+
+            Style controlStyle = FindResource("BottomStyle") as Style;
+
+            for (int i = 0; i < totalItems; i++)
+            {
+                var uiElement = new TextBlock
+                {
+                    Width = itemWidth,
+                    Height = heightCoef * values.ElementAt(i),
+                    Background = i % 2 == 0 ? Brushes.Black: Brushes.Red,
+                    Foreground = i % 2 != 0 ? Brushes.Black: Brushes.Red,
+                    Style = controlStyle,  
+                    Text = values.ElementAt(i).ToString(),
+                };
+
+
+                target.Children.Add(uiElement);
+            }           
         }
     }
 }
