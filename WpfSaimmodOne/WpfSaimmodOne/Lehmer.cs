ca6deeaa237ad94b190310bc7cf57432b8fce81f
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-09-09 21:42:26 +0300

    Improved parameter generator

--- WpfSaimmodOne/WpfSaimmodOne/Lehmer.cs
+++ WpfSaimmodOne/WpfSaimmodOne/Lehmer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -67,58 +68,57 @@ namespace WpfSaimmodOne
         // a < R[0] < m
         public static (uint multiplier, uint initialValue, uint divider) GenerateParameters()
         {
-            uint multiplier;
-            uint initialValue;
-            uint divider;
+            bool CheckBinaryRepresentation(uint val)
+            {
+                const int ENOUGH_ONES_IN_REPRESENTATION = 16; // 
+                return Convert.ToString(val, 2).Count(i => i == '1') >= ENOUGH_ONES_IN_REPRESENTATION;
+            }
 
+            uint GenerateRandomValue(
+                uint intervalBegin, 
+                uint intervalEnd, 
+                Random random,
+                IEnumerable<uint> deniedValues,
+                bool checkPrime = false,
+                bool checkBits = false)
+            {
+                while (true)
+                {
+                    uint val = UIntRandom.GetValue(intervalBegin, intervalEnd, random);
+
+                    if ((deniedValues != null && deniedValues.Contains(val))
+                        || (checkBits && !CheckBinaryRepresentation(val))
+                        || (checkPrime && !PrimeTests.IsPrime(val)))
+                    {
+                        continue;
+                    }
+
+                    return val;
+                }
+            }
+                   
             var rand = new Random();
+            IEnumerable<uint> deniedVals = new List<uint>();
 
             // set values bounds
-            var data = new (uint intervalBegin, uint intervalEnd, uint? generatedValue)[3] {
-                ((uint)(0.75 * (uint)Math.Pow(2, 31)), (uint)(1.5 * (uint)Math.Pow(2, 31)), null),
-                ((uint)(1.5 * (uint)Math.Pow(2, 31)), uint.MaxValue, null),
-                ((uint)(1.5 * (uint)Math.Pow(2, 31)), uint.MaxValue, null),
+            var data = new (uint intervalBegin, uint intervalEnd, bool prime, bool bitCheck, uint value)[3] {
+                ((uint)(1.5 * (uint)Math.Pow(2, 31)), uint.MaxValue, true, false, 0), // divider              
+                ((uint)(0.75 * (uint)Math.Pow(2, 31)), (uint)(1.5 * (uint)Math.Pow(2, 31)), true, false, 0), // mult
+                ((uint)Math.Pow(2, 30), uint.MaxValue, true, true, 0), // initialValue
             };
 
             // values generation 
             for (int i = 0; i < data.Length; i++)
             {
-                while (data[i].generatedValue == null)
-                {
-                    uint val = UIntRandom.GetValue(data[i].intervalBegin, data[i].intervalEnd, rand);
+                data[i].value = GenerateRandomValue(
+                    data[i].intervalBegin, data[i].intervalEnd, 
+                    rand, deniedVals, 
+                    data[i].prime, data[i].bitCheck);
 
-                    var allValues = data.Select(d => d.generatedValue).Where(e => e != null).ToList();
-                    allValues.Add(val);
-                    /// check if this value {val} is already represented
-                    if (allValues.Distinct().Count() == allValues.Count())
-                    {
-                        if (PrimeTests.IsPrime(val))
-                        {
-                            data[i].generatedValue = val;
-                        }
-                    }
-                }
+                deniedVals.Append(data[i].value);
             }
 
-            #region sort values
-            //a < R[0] < m
-
-            multiplier = (uint)data[0].generatedValue;
-
-            if (data[1].generatedValue < data[2].generatedValue)
-            {
-                initialValue = (uint)data[1].generatedValue;
-                divider = (uint)data[2].generatedValue;
-            }
-            else
-            {
-                initialValue = (uint)data[2].generatedValue;
-                divider = (uint)data[1].generatedValue;
-            }
-
-            #endregion
-
-            return (multiplier, initialValue, divider);
+            return (data[1].value, data[2].value, data[0].value);
         }
 
     }
