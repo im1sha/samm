2813c8e94c1089ff497816837b0c7ee568d1b892
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-09-10 23:10:32 +0300

    view updater created

--- WpfSaimmodOne/WpfSaimmodOne/ViewModels/AppViewModel.cs
+++ WpfSaimmodOne/WpfSaimmodOne/ViewModels/AppViewModel.cs
@@ -28,9 +28,9 @@ namespace WpfSaimmodOne.ViewModels
             get
             {
                 return _generateCommand ??
-                    (_generateCommand = new InteractCommand(o =>
+                    (_generateCommand = new InteractCommand(stack =>
                     {
-                        
+                       
                     }));
             }
         }
@@ -41,12 +41,73 @@ namespace WpfSaimmodOne.ViewModels
             get
             {
                 return _autogenerateCommand ??
-                    (_autogenerateCommand = new InteractCommand(o =>
+                    (_autogenerateCommand = new InteractCommand(stack =>
                     {
 
                     }));
             }
         }
+
+        //private void AutoGenerate(object sender, RoutedEventArgs e)
+        //{
+        //    uint mul;
+        //    uint ini;
+        //    uint div;
+        //    bool correctData;
+        //    bool validPeriod;
+
+        //    Mediator md;
+        //    do
+        //    {
+        //        (mul, ini, div) = Lehmer.GenerateRandomParameters();
+        //        md = new Mediator(new UniformDistribution(div), new Lehmer(mul, ini, div));
+        //        md.Initialize();
+        //        (correctData, _) = md.EstimateDistribution();
+        //        (validPeriod, _, _) = md.EstimatePeriod();
+        //    } while (!correctData || !validPeriod);
+
+        //    _divider.Text = div.ToString();
+        //    _initialValue.Text = ini.ToString();
+        //    _multiplier.Text = mul.ToString();
+
+        //    RunCore(md);
+        //}
+
+        //private void ManualRun(object sender, RoutedEventArgs e)
+        //{
+        //    #region form data parsing
+
+        //    uint GetUIntContent(string str)
+        //    {
+        //        if (!uint.TryParse(str, out uint res))
+        //        {
+        //            return 0;
+        //        }
+        //        return res;
+        //    }
+        //    var mul = GetUIntContent(_multiplier.Text);
+        //    var ini = GetUIntContent(_initialValue.Text);
+        //    var div = GetUIntContent(_divider.Text);
+
+        //    #endregion
+
+        //    var md = new Mediator(new UniformDistribution(div), new Lehmer(mul, ini, div));
+        //    md.Initialize();
+        //    RunCore(md);
+        //}
+
+        //private void RunCore(Mediator md)
+        //{
+        //    DrawBarChart(_panel, md.GetChart());
+
+        //    (double expectedValue, double variance, double standardDeviation)
+        //        = md.GetNormalizedStatistics();
+
+        //    (_, var estimation) = md.EstimateDistribution();
+        //    (_, var period, var aperiodicity) = md.EstimatePeriod();
+
+        //    ShowStatistics(expectedValue, variance, standardDeviation, estimation, period, aperiodicity);
+        //}
 
         #endregion
 
@@ -148,116 +209,5 @@ namespace WpfSaimmodOne.ViewModels
             }
         }
         #endregion
-
-        //private void AutoGenerate(object sender, RoutedEventArgs e)
-        //{
-        //    uint mul;
-        //    uint ini;
-        //    uint div;
-        //    bool correctData;
-        //    bool validPeriod;
-
-        //    Mediator md;
-        //    do
-        //    {
-        //        (mul, ini, div) = Lehmer.GenerateRandomParameters();
-        //        md = new Mediator(new UniformDistribution(div), new Lehmer(mul, ini, div));
-        //        md.Initialize();
-        //        (correctData, _) = md.EstimateDistribution();
-        //        (validPeriod, _, _) = md.EstimatePeriod();
-        //    } while (!correctData || !validPeriod);
-
-        //    _divider.Text = div.ToString();
-        //    _initialValue.Text = ini.ToString();
-        //    _multiplier.Text = mul.ToString();
-
-        //    RunCore(md);
-        //}
-
-        //private void ManualRun(object sender, RoutedEventArgs e)
-        //{
-        //    #region form data parsing
-
-        //    uint GetUIntContent(string str)
-        //    {
-        //        if (!uint.TryParse(str, out uint res))
-        //        {
-        //            return 0;
-        //        }
-        //        return res;
-        //    }
-        //    var mul = GetUIntContent(_multiplier.Text);
-        //    var ini = GetUIntContent(_initialValue.Text);
-        //    var div = GetUIntContent(_divider.Text);
-
-        //    #endregion
-
-        //    var md = new Mediator(new UniformDistribution(div), new Lehmer(mul, ini, div));
-        //    md.Initialize();
-        //    RunCore(md);
-        //}
-
-        //private void RunCore(Mediator md)
-        //{
-        //    DrawBarChart(_panel, md.GetChart());
-
-        //    (double expectedValue, double variance, double standardDeviation)
-        //        = md.GetNormalizedStatistics();
-
-        //    (_, var estimation) = md.EstimateDistribution();
-        //    (_, var period, var aperiodicity) = md.EstimatePeriod();
-
-        //    ShowStatistics(expectedValue, variance, standardDeviation, estimation, period, aperiodicity);
-        //}
-
-        //private void DrawBarChart(Panel target, IEnumerable<uint> values)
-        //{
-        //    target.Children.Clear();
-
-        //    double heightCoef = target.ActualHeight / values.Max();
-        //    double itemWidth = target.ActualWidth / values.Count();
-
-        //    int totalItems = values.Count();
-
-        //    Style controlStyle = FindResource("BottomStyle") as Style;
-
-        //    int rowLength = 999.ToString().Length;
-        //    for (int i = 0; i < totalItems; i++)
-        //    {
-        //        string content = values.ElementAt(i).ToString();
-
-        //        if (content.Length > rowLength)
-        //        {
-        //            for (int j = content.Length - rowLength; j > 0; j -= rowLength)
-        //            {
-        //                content = content.Insert(j, "\n");
-        //            }
-        //        }
-
-        //        var uiElement = new TextBlock
-        //        {
-        //            Width = itemWidth,
-        //            Height = heightCoef * values.ElementAt(i),
-        //            Background = i % 2 == 0 ? Brushes.Black : Brushes.DarkGray,
-        //            Foreground = i % 2 != 0 ? Brushes.Black : Brushes.LightGray,
-        //            Style = controlStyle,
-        //            Text = content,
-        //        };
-
-        //        target.Children.Add(uiElement);
-        //    }
-        //}
-
-        //private void ShowStatistics(double expectedValue, double variance, double standardDeviation,
-        //    double estimation, int period, int aperiodicity)
-        //{
-        //    _expectedValue.Content = "M: " + expectedValue;
-        //    _variance.Content = "D: " + variance;
-        //    _standardDeviation.Content = "σ: " + standardDeviation;
-        //    _estimation.Content = "est: " + estimation;
-        //    _period.Content = "P: " + period;
-        //    _aperiodicity.Content = "L: " + aperiodicity;
-        //}
-    
     }
 }
