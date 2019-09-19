7e2c8463844f2d1b04137b9b91dd45d3a66d7a76
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-09-19 13:06:15 +0300

    [bd] lab1task1 added

---
+++ bd/lab1/lab1task1.sql
@@ -0,0 +1,26 @@
+BACKUP DATABASE [AdventureWorks2012] TO DISK = 'E:\backup.bak'
+WITH FORMAT
+GO
+
+USE [master];
+GO
+
+DECLARE @dbId INT;
+
+SET @dbId = DB_ID(N'AdventureWorks2012');
+
+IF @dbId IS NOT NULL
+BEGIN;
+
+	 ALTER DATABASE [AdventureWorks2012]
+	 SET SINGLE_USER
+	 WITH
+	 ROLLBACK IMMEDIATE;
+
+	 DROP DATABASE [AdventureWorks2012];
+
+	 RESTORE DATABASE [AdventureWorks2012]
+	 FROM DISK = 'E:\backup.bak'
+END;
+GO
+
