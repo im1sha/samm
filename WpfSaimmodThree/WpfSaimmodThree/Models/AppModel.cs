1fe1005f916b6ad5625797a72787214392145565
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-10-08 01:17:37 +0300

    AppViewModel, UI added, AppModel started

---
+++ WpfSaimmodThree/WpfSaimmodThree/Models/AppModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace WpfSaimmodThree.Models
+{
+    internal class AppModel
+    {
+        private readonly double _busyProbability1;
+        private readonly double _busyProbability2;
+
+        public AppModel(double probability1, double probability2)
+        {
+            _busyProbability1 = probability1;
+            _busyProbability2 = probability2;
+        }
+
+        private readonly Random _random1 = new Random();
+        private readonly Random _random2 = new Random(42);
+
+        private bool ChannelIsBusy1 => _random1.NextDouble() <= _busyProbability1;
+        private bool ChannelIsBusy2 => _random2.NextDouble() <= _busyProbability2;
+
+
+    }
+}
--- WpfSaimmodThree/WpfSaimmodThree/ViewModels/AppViewModel.cs
+++ WpfSaimmodThree/WpfSaimmodThree/ViewModels/AppViewModel.cs
@@ -1,12 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Globalization;
+using System.Runtime.CompilerServices;
+using WpfSaimmodThree.Models;
 
 namespace WpfSaimmodThree.ViewModels
 {
-    class AppViewModel
+    internal class AppViewModel : INotifyPropertyChanged
     {
+        #region INotifyPropertyChanged 
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+
+        #endregion
+
+        #region localization
+
+        private static readonly CultureInfo APP_LOCALIZATION = new CultureInfo("en-US");
+
+        #endregion
+
+        #region input 
+
+        private double _probability1 = 0.5;
+        public string Probability1
+        {
+            get => _probability1.ToString(APP_LOCALIZATION);
+            set
+            {
+                if (!double.TryParse(value, out _))
+                {
+                    return;
+                }
+                _probability1 = Convert.ToDouble(value, APP_LOCALIZATION);
+                OnPropertyChanged();
+            }
+        }
+
+        private double _probability2 = 0.5;
+        public string Probability2
+        {
+            get => _probability2.ToString(APP_LOCALIZATION);
+            set
+            {
+                if (!double.TryParse(value, out _))
+                {
+                    return;
+                }
+                _probability2 = Convert.ToDouble(value, APP_LOCALIZATION);
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region labels
+
+        private double _failProbability;
+        public string FailProbability
+        {
+            get => $"P(fail): {_failProbability}";
+            set
+            {
+                _failProbability = Convert.ToDouble(value, APP_LOCALIZATION);
+                OnPropertyChanged();
+            }
+        }
+
+        private double _bandwidth;
+        public string Bandwidth
+        {
+            get => $"A: {_bandwidth}";
+            set
+            {
+                _bandwidth = Convert.ToDouble(value, APP_LOCALIZATION);
+                OnPropertyChanged();
+            }
+        }
+
+        private double _queueLength;
+        public string QueueLength
+        {
+            get => $"L(queue): {_queueLength}";
+            set
+            {
+                _queueLength = Convert.ToDouble(value, APP_LOCALIZATION);
+                OnPropertyChanged();
+            }
+        }
+
+        #endregion
+
+        #region commands
+
+        private InteractCommand _initializeCommand;
+        public InteractCommand InitializeCommand => _initializeCommand ??
+            (_initializeCommand = new InteractCommand((o) =>
+            {
+                var model = new AppModel(_probability1, _probability2);
+
+                // 
+                //
+                //
+            }));
+
+        #endregion
     }
 }
