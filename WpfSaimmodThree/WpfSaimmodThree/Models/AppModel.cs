ecb089cabae330ef859fd8d0be751bcbd34c06c0
Mikhail Ovchelupov <mikefoxx2000@ya.ru>
2019-10-08 22:59:04 +0300

    states machine added

--- WpfSaimmodThree/WpfSaimmodThree/Models/AppModel.cs
+++ WpfSaimmodThree/WpfSaimmodThree/Models/AppModel.cs
@@ -1,24 +1,270 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace WpfSaimmodThree.Models
 {
     internal class AppModel
     {
-        private readonly double _busyProbability1;
-        private readonly double _busyProbability2;
+        #region given data
 
-        public AppModel(double probability1, double probability2)
+        public double BusyProbability1 { get; }
+        public double BusyProbability2 { get; }
+        public int TotalTacts { get; }
+
+        #endregion
+
+        #region stored statistics
+
+        public int TotalGenerated { get; private set; }
+        public int TotalProcessed { get; private set; }
+        public int TotalFailures { get; private set; }
+
+        private readonly List<State> _systemStates = new List<State>();
+
+        #endregion
+
+        #region ctor
+        public AppModel(double probability1, double probability2, int totalTacts = 1_000_000)
         {
-            _busyProbability1 = probability1;
-            _busyProbability2 = probability2;
+            BusyProbability1 = probability1;
+            BusyProbability2 = probability2;
+            TotalTacts = totalTacts;
         }
+        #endregion
 
-        private readonly Random _random1 = new Random();
+        #region current channel state genetation
+
+        private readonly Random _random1 = new Random(123456);
         private readonly Random _random2 = new Random(42);
 
-        private bool ChannelIsBusy1 => _random1.NextDouble() <= _busyProbability1;
-        private bool ChannelIsBusy2 => _random2.NextDouble() <= _busyProbability2;
+        private bool GetChannelIsBusy1()
+        {
+            return _random1.NextDouble() <= BusyProbability1;
+        }
 
+        private bool GetChannelIsBusy2()
+        {
+            return _random2.NextDouble() <= BusyProbability2;
+        }
 
+        #endregion
+
+        #region calculated statistics
+
+        // A in [0.0, 1.0]
+        public double GetBandwidth()
+        {
+            return TotalProcessed / (double)TotalTacts;
+        }
+
+        // Lqueue in [0.0, 2.0]
+        public double GetAverageQueueLength()
+        {
+            return _systemStates.Average(s => (double)s.CurrentQueueLength);
+        }
+
+        // Pfail
+        public double GetFailureProbability()
+        {
+            return TotalFailures / (double)TotalGenerated;
+        }
+
+        #endregion
+
+        public void Run()
+        {
+            int GetNextTacts(int currentTacts)
+            {
+                if (currentTacts == 2)
+                {
+                    return 1;
+                }
+                else if (currentTacts == 1)
+                {
+                    return 2;
+                }
+                else
+                {
+                    throw new ArgumentException(nameof(currentTacts));
+                }
+            }
+
+            //currentQueueLength={0, 1, 2}
+            //currentTacts={1,2}
+            (int Length, bool Failed) GetNextQueueLength(bool IsBusyChannel1,
+                int currentQueueLength, int currentTacts, int maxQueueLength = 2)
+            {
+                if (maxQueueLength > currentQueueLength)
+                {
+                    throw new ArgumentException();
+                }
+                if (IsBusyChannel1 && currentTacts == 1 && currentQueueLength == maxQueueLength)
+                {
+                    return (maxQueueLength, true);
+                }
+                else if (IsBusyChannel1 && currentTacts == 1)
+                {
+                    return (++currentQueueLength, false);
+                }
+                else
+                {
+                    return (currentQueueLength, false);
+                }
+            }
+
+            //
+            bool channelIsBusy1 = false;
+            bool channelIsBusy2 = false;
+            int currentQueueLength = 0;
+            int tactsToNewItem = 2;
+            //
+
+            for (int i = 0; i < TotalTacts; i++)
+            {
+                bool isFailed = false;
+
+                #region unused
+                //currentState = new State(channelIsFree1, channelIsFree2, 
+                //    currentQueueLength, tactsToNewItem);
+                //_systemStates.Add(currentState);
+                //++TotalGenerated;
+                #endregion
+
+                (bool, bool) generatedIsBusy = (GetChannelIsBusy1(), GetChannelIsBusy2());
+
+                switch ((tactsToNewItem, currentQueueLength, channelIsBusy1, channelIsBusy2))
+                {
+                    case (2, 0, false, false):
+                        // no channels changed
+                        // -> (x, x, false, false)
+                        break;
+                    case (1, 0, false, false):
+                        channelIsBusy1 = true;
+                        // -> (x, x, true, false)
+                        break;
+                    case (2, 0, true, false):
+                    case (1, 0, true, false):
+                    case (2, 1, true, false):
+                    case (1, 1, true, false):
+                    case (2, 2, true, false):
+                        // IsBusy: true == 1
+                        if (generatedIsBusy.Item1)
+                        {
+                            // no channels changed
+                            // -> (x, x, true, false)
+                        }
+                        else
+                        {
+                            channelIsBusy2 = true;
+                            if (currentQueueLength == 0 && tactsToNewItem == 2)
+                            {
+                                channelIsBusy1 = false;
+                                // -> (x, x, false, true)
+                            }
+                            else
+                            {
+                                channelIsBusy1 = true;
+                                // -> (x, x, true, true)
+                            }
+                        }
+                        break;
+                    case (1, 2, true, false):
+                        if (generatedIsBusy.Item1)
+                        {
+                            // no channels changed
+                            // -> (x, x, true, false)
+                        }
+                        else
+                        {
+                            channelIsBusy2 = true;
+                            // -> (x, x, true, true)
+                        }
+                        break;
+                    case (1, 0, false, true):
+                        channelIsBusy1 = true;
+                        if (generatedIsBusy.Item2)
+                        {
+                            channelIsBusy2 = true;
+                            // -> (x, x, true, true)
+                        }
+                        else
+                        {
+                            channelIsBusy2 = false;
+                            // -> (x, x, true, false)
+                        }
+                        break;
+                    case (2, 0, true, true):
+                        if (!generatedIsBusy.Item1)
+                        {
+                            channelIsBusy1 = false;
+                            // -> (x, x, false, true)
+                        }
+                        else if (generatedIsBusy.Item1 && generatedIsBusy.Item2)
+                        {                         
+                            // no channels changed
+                            // -> (x, x, true, true)
+                        }
+                        else
+                        {
+                            channelIsBusy2 = false;
+                            // -> (x, x, true, true)
+                        }
+                        break;
+                    case (1, 0, true, true):
+                    case (2, 1, true, true):
+                    case (1, 1, true, true):
+                        if ((!generatedIsBusy.Item1) 
+                            || (generatedIsBusy.Item1 && generatedIsBusy.Item2))
+                        {
+                            // no channels changed
+                            // -> (x, x, true, true)
+                        }
+                        else
+                        {
+                            channelIsBusy2 = false;
+                            // -> (x, x, true, true)
+                        }
+                        break;
+                    case (2, 2, true, true):
+                        if ((generatedIsBusy.Item1 && generatedIsBusy.Item2)
+                            || (!generatedIsBusy.Item1))
+                        {
+                            // no channels changed
+                            // -> (x, x, true, true)
+                        }
+                        else 
+                        {
+                            channelIsBusy2 = false;
+                            // -> (x, x, true, false)
+                        }
+                        break;
+                    case (1, 2, true, true):
+                        if (generatedIsBusy.Item1 && !generatedIsBusy.Item2)
+                        {
+                            channelIsBusy2 = false;
+                            // -> (x, x, true, false)
+                        }
+                        else
+                        {
+                            // no channels changed
+                            // -> (x, x, true, true)
+                        }
+                        break;
+                    default:
+                        break;
+                }
+                // update currentQueueLength
+                (currentQueueLength, isFailed) = GetNextQueueLength(channelIsBusy1,
+                    currentQueueLength, tactsToNewItem);
+                if (isFailed)
+                {
+                    ++TotalFailures;
+                }
+
+                // update tactsToNewItem
+                tactsToNewItem = GetNextTacts(tactsToNewItem);
+            }
+        }
     }
 }
---
+++ WpfSaimmodThree/WpfSaimmodThree/Models/State.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace WpfSaimmodThree.Models
+{
+    internal class State
+    {
+        public bool ChannelIsBusy1 { get; }
+        public bool ChannelIsBusy2 { get; }
+
+        // {0, 1, 2}
+        public int CurrentQueueLength { get; }
+        private static readonly int[] POSSIBLE_QUEUE_LENGTH = new[] { 0, 1, 2 };
+
+        // {1, 2}
+        public int TactsToNewItem { get; }
+        private static readonly int[] POSSIBLE_TACTS = new[] { 2, 1 };
+
+        public State(int tactsToNewItem, int currentQueueLength, bool channelIsBusy1, bool channelIsBusy2)
+        {
+            if (!POSSIBLE_QUEUE_LENGTH.Contains(currentQueueLength))
+            {
+                throw new ArgumentException(nameof(currentQueueLength));
+            }
+            if (!POSSIBLE_TACTS.Contains(tactsToNewItem))
+            {
+                throw new ArgumentException(nameof(tactsToNewItem));
+            }
+
+            ChannelIsBusy1 = channelIsBusy1;
+            ChannelIsBusy2 = channelIsBusy2;
+            CurrentQueueLength = currentQueueLength;
+            TactsToNewItem = tactsToNewItem;
+        }
+
+        public State((int TactsToNewItem, int CurrentQueueLength, bool ChannelIsBusy1, bool ChannelIsBusy2) state) 
+            : this(state.TactsToNewItem, state.CurrentQueueLength, state.ChannelIsBusy1, state.ChannelIsBusy2)
+        {             
+        }
+    }
+}
